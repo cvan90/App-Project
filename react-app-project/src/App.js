32baf96586c42306053d151454fbbe9fa3d6a369
cvan90 <cvanv90@yahoo.com>
2025-03-21 13:21:12 -0400

    added txt search box

--- react-app-project/src/App.js
+++ react-app-project/src/App.js
@@ -3,13 +3,14 @@
 function App() {
 
 
-  
+
   return (
     <>
     <div>
       <h1>To-Do List</h1>
     </div>
 <div>
+  <input type="text" placeholder="Enter Text to Search "></input>
 <button>Add</button>
 </div>
 <div>
