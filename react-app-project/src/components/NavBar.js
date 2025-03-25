074373254b12b73951754d6918e137ada6a3f216
cvan90 <cvanv90@yahoo.com>
2025-03-25 14:29:22 -0400

    created navbar

---
+++ react-app-project/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+function NavBar(){
+    return (
+        <>
+        <nav>
+            <link to="/">Home</link>
+            <link to="/about">About</link>
+        </nav>
+        </>
+    )
+}
+export default NavBar;
