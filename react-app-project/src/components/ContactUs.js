4f09b29a02ceaf412cd363c6f75de39f72754845
cvan90 <cvanv90@yahoo.com>
2025-03-27 18:18:42 -0400

    coded in form for contact page

--- react-app-project/src/components/ContactUs.js
+++ react-app-project/src/components/ContactUs.js
@@ -2,6 +2,14 @@ function ContactUS() {
     return (
         <>
         <p>Contact Us</p>
+        <form>
+            <div>
+                <span>First Name</span><input type="text" placeholder="First Name"/>
+                <span>Last Name</span><input type="text" placeholder="Last Name"/>
+                <span>Email</span><input type="text" placeholder="Email Address"/>
+                <button>Submit</button>
+            </div>
+        </form>
         </>
     )
 }
--- react-app-project/src/components/NavBar.js
+++ react-app-project/src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar(){
         <>
         <nav>
             <Link to="/">Home</Link>{" | "}
-            <Link to="/about">About</Link>
+            <Link to="/ContactUs">Contact Us</Link>
         </nav>
         </>
     );
