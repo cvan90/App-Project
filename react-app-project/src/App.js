755badc4bc44e1633a371266d87aafd203915ba8
cvan90 <cvanv90@yahoo.com>
2025-03-28 15:20:30 -0400

    added center page style

--- react-app-project/src/App.js
+++ react-app-project/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home"
 import {HashRouter, Routes, Route} from "react-router-dom"
 import ContactUS from "./components/ContactUs"
 import NavBar from "./components/NavBar";
+import "./App.css"
 function App() {
 
   return (
