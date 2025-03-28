d2a612aaf42dd12acff35f3fd4508c5de47223ca
cvan90 <cvanv90@yahoo.com>
2025-03-28 12:03:28 -0400

    added comment box

--- react-app-project/src/components/ContactUs.js
+++ react-app-project/src/components/ContactUs.js
@@ -7,6 +7,8 @@ function ContactUS() {
                 <span>First Name:</span><input type="text" placeholder="First Name"/><br/>
                 <span>Last Name:</span><input type="text" placeholder="Last Name"/><br/>
                 <span>Email:</span><input type="text" placeholder="Email Address"/><br/>
+                <span>Comments:</span>
+                <br/><textarea rows={5} cols={30} placeholder="Comment Here"/><br/>
                 <button>Submit</button>
             </div>
         </form>
