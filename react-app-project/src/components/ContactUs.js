fd362544792d4a689ce3c43c9edfd0ae00f96964
cvan90 <cvanv90@yahoo.com>
2025-03-30 12:36:52 -0400

    fixed onclick button

--- react-app-project/src/components/ContactUs.js
+++ react-app-project/src/components/ContactUs.js
@@ -6,10 +6,9 @@ function ContactUS() {
     const txtfirstname = useRef("");
     const txtlastname = useRef("");
     const txtemail = useRef("");
-    const txtphone = useRef("");
     const txtcomment = useRef("");
   
-const btnSubmitForm = (event)=>
+const btnsubmitform = (event)=>
         {
             let _msg = "";
     
@@ -19,40 +18,46 @@ const btnSubmitForm = (event)=>
             console.log(_msg)
     
             if(txtfirstname.current.value == null | txtfirstname.current.value.trim().length == 0) {
-              _msg = "## please enter a valid first name"
+              _msg = "---Please Enter First Name---"
               outputDiv.current.innerText = _msg;
               return false;  
             }
+            if(txtlastname.current.value == null | txtlastname.current.value.trim().length == 0) {
+                _msg = "---Please Enter Last Name---"
+                outputDiv.current.innerText = _msg;
+                return false;  
+              }
     
             if(txtemail.current.value == null | txtemail.current.value.trim().length == 0) {
-              _msg = "## please enter a valid email"
+              _msg = "---Please Enter a Valid Email---"
               outputDiv.current.innerText = _msg;
               return false;  
             }
     
             if(txtcomment.current.value == null | txtcomment.current.value.trim().length == 0) {
-              _msg = "## please enter a valid comment"
+              _msg = "---please enter a valid comment---"
               outputDiv.current.innerText = _msg;
               return false; 
             }
     
-            outputDiv.current.innerText = "form submitted"
+            outputDiv.current.innerText = "---Form Submitted---"
     
-            _msg = `## form submit: ${txtfirstname.current.value} ${txtlastname.current.value} - ${txtemail.current.value}
+            _msg = `---Form submited---: ${txtfirstname.current.value}- ${txtlastname.current.value} - ${txtemail.current.value}
             - ${txtcomment.current.value}`
            
             console.log(_msg)
     
-            //clear fields
+            
             txtfirstname.current.value = "";
             txtlastname.current.value = "";
             txtemail.current.value = "";
             txtcomment.current.value = "";
     
-            //field focus
+            
             txtfirstname.current.focus()
     
         }
+
       useEffect(()=>{
       console.log("---Contact-Us Page Loaded---")
     
@@ -61,17 +66,23 @@ const btnSubmitForm = (event)=>
 
     return (
         <>
-        <p>Contact Us</p>
+        <div>
+        <h2>Contact Us</h2>
+        <div ref={outputDiv}></div><br/>
         <form>
             <div>
-                <span>First Name:</span><input ref={txtfirstname} type="text" placeholder="First Name"/><br/>
-                <span>Last Name:</span><input ref={txtlastname} type="text" placeholder="Last Name"/><br/>
-                <span>Email:</span><input ref={txtemail} type="text" placeholder="Email Address"/><br/>
+                <span>First Name:</span><input ref={txtfirstname} type="text" placeholder="First Name"/>
+                <br/>
+                <span>Last Name:</span><input ref={txtlastname} type="text" placeholder="Last Name"/>
+                <br/>
+                <span>Email:</span><input ref={txtemail} type="text" placeholder="Email Address"/>
+                <br/>
                 <span>Comments:</span>
                 <br/><textarea ref={txtcomment} rows={5} cols={30} placeholder="Comment Here"/><br/>
-                <button onclick={btnSubmitForm}>Submit</button>{" "}
+                <button onClick={(e)=>btnsubmitform(e)}>Submit</button>{" "}
             </div>
         </form>
+        </div>
         </>
     )
 }
